5acf57809c2179e4ac711cb0bbd06a00471595bd
SaiGovardhana <191fa04292@gmail.com>
2023-03-08 00:40:07 +0530

    Changed Resume Model

--- src/main/models/models.ts
+++ src/main/models/models.ts
@@ -1,5 +1,16 @@
 export type UserModel= {name:string,email:string,password:string}
 export type BasicObject={[key:string]:string|boolean|number}
-export type ResumeModel={headers:{[key:string]:HeaderModel},sections:SectionModel[]}
+export type ResumeModel={headers:{[key:string]:HeaderModel},sections:SectionModel[],resumeOptions?:ResumeOptionsModel}
 export type HeaderModel={name:string,headerContent:string}
 export type SectionModel={name:string,headerContent:string,sectionContent:string}
+export type ResumeOptionsModel=
+{
+    bodyBackgroundColor:string;
+    bodyTextColor:string;
+    headerBackgroundColor:string,
+    headerTextColor:string,
+    showPhone:boolean,
+    showEmail:boolean,
+    showLocation:boolean
+
+};
