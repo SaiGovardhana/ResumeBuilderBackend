30da6010a63a50e71c855aea8e5d3f9477f83d67
SaiGovardhana <191fa04292@gmail.com>
2023-03-11 13:12:55 +0530

    Final Change

--- src/main/fetchPdf/FetchPDF.ts
+++ src/main/fetchPdf/FetchPDF.ts
@@ -4,7 +4,7 @@ import { sendMail } from '../mail/SendMail.js';
 import { getResume } from '../DAO/ResumeDAO.js';
 import { ResumeModel } from '../models/models.js';
 dotenv.config();
-
+import {readFile} from 'fs/promises'
 //FIX FOR MOBILE HOTSPOT
 import { setDefaultResultOrder } from "dns";
 setDefaultResultOrder("ipv4first");
@@ -12,7 +12,10 @@ export async function sendPDFToEmail(resumeId:string,userEmail:string) {
 
   try{
   
-      const browser = await puppeteer.launch();
+      const browser = await puppeteer.launch(
+
+       { args: ['--font-render-hinting=none', '--font-render-hinting-size=medium', `--font-priority=1,2,3,4,5,6,7,8,9,10`, '--font-family="DejaVu Serif Condensed"']
+  });
       const page = await browser.newPage();
       await page.goto(`http://${process.env.FRONT_END}/puppeter?resumeId=${resumeId}`, {waitUntil: 'networkidle2'});
       setTimeout(async()=>
@@ -26,9 +29,12 @@ export async function sendPDFToEmail(resumeId:string,userEmail:string) {
           if(resumeModel!=null && resumeModel.resumeOptions!=null)
           {   
             console.log("Here In Text "+resumeModel.resumeOptions.bodyBackgroundColor)
-            await page.addStyleTag({content:`body,html{background:${resumeModel.resumeOptions.bodyBackgroundColor} !important;}`});
+  
+            await page.addStyleTag({content:`body,html{padding:10px;background:${resumeModel.resumeOptions.bodyBackgroundColor} !important;}`});
+           
           }
-        await page.pdf({path: `generated/${resumeId}.pdf`, format: 'A4',printBackground:true});
+
+        await page.pdf({path: `generated/${resumeId}.pdf`,format: 'A4',printBackground:true});
         await browser.close();
         sendMail(userEmail,resumeId);
         }
